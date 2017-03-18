dbb60194fc5e505fe615726f7cfcd88ff57aa603
pcaday <pcaday@users.noreply.github.com>
2017-03-18 09:55:15 -0500

    Add Makefile for x86 hosted front ends

--- frontends/x86-hosted-unix/make-x86-osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-nasm -f macho -g --prefix _ -i../../cores/x86/ x86-core-wrap.S
-gcc -c -O -g -m32 -I.. unix-x86-cl.c
-gcc -c -O -g -m32 ../preprocessor.c
-gcc unix-x86-cl.o preprocessor.o x86-core-wrap.o -m32 -o unix-x86-cl
--- frontends/x86-hosted/make-x86-win.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CC="i686-w64-mingw32-gcc"
-
-nasm -f win32 -g -i../../cores/x86/ -o x86-core-wrap.o --prefix _ x86-core-wrap.S
-$CC -c -O -g -m32 -I.. win-x86-cl.c
-$CC -c -O -g -m32 ../preprocessor.c
-$CC win-x86-cl.o preprocessor.o x86-core-wrap.o -m32 -o win-x86-cl.exe
